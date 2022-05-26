bc591cb220bd1dd6f66cfce8733d37c4d98b313e
wesley Penachia <wesleyikiko@hotmail.com>
2022-05-25 22:11:12 -0300

    Projeto criado

---
+++ src/main/java/com/me/portalchamados/PortaldechamadosApplication.java
@@ -0,0 +1,13 @@
+package com.me.portalchamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaldechamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaldechamadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/portalchamados/PortaldechamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.portalchamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaldechamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
