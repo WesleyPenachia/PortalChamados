edf113b2727f1e6de6805ceef0e51b6ff06bc7b3
wesley Penachia <wesleyikiko@hotmail.com>
2022-05-25 23:02:19 -0300

    annotations

--- src/main/java/com/me/portalchamados/domain/Cliente.java
+++ src/main/java/com/me/portalchamados/domain/Cliente.java
@@ -1,18 +1,29 @@
 package com.me.portalchamados.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa{
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.me.portalchamados.domain.enums.Perfil;
+
+@Entity
+public class Cliente extends Pessoa implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/me/portalchamados/domain/Pessoa.java
+++ src/main/java/com/me/portalchamados/domain/Pessoa.java
@@ -1,21 +1,45 @@
 package com.me.portalchamados.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.me.portalchamados.domain.enums.Perfil;
 
-public abstract class Pessoa {
-
+@Entity
+public abstract class Pessoa implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 	protected String nome;
+	
+	@Column(unique = true)
 	protected String cpf;
+	
+	@Column(unique = true)
 	protected String email;
 	protected String senha;
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "PERFIS")
 	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 	
 	public Pessoa() {
