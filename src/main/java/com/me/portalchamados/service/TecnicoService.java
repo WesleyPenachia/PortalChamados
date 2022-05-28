b227aea7b316e7d46937e3fa0c4031275064d10e
wesley Penachia <wesleyikiko@hotmail.com>
2022-05-28 00:39:26 -0300

    delete

--- src/main/java/com/me/portalchamados/service/TecnicoService.java
+++ src/main/java/com/me/portalchamados/service/TecnicoService.java
@@ -49,6 +49,16 @@ public class TecnicoService {
 		return pessoaRepository.save(oldObj);
 	}
 	
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+		}
+	
+		tecnicoRepository.deleteById(id);			
+	}
+	
+	
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
