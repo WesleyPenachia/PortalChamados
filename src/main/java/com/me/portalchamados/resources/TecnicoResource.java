fc55c2b4617ccd54a4b5953ec6c70f129685d827
wesley Penachia <wesleyikiko@hotmail.com>
2022-05-27 22:48:01 -0300

    findAll

--- src/main/java/com/me/portalchamados/resources/TecnicoResource.java
+++ src/main/java/com/me/portalchamados/resources/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.me.portalchamados.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,4 +26,12 @@ public class TecnicoResource {
 		Tecnico obj = this.tecnicoService.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll() {
+		List<Tecnico> list = tecnicoService.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
 }
--- src/main/java/com/me/portalchamados/service/TecnicoService.java
+++ src/main/java/com/me/portalchamados/service/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.me.portalchamados.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: id: " + id));
+	}
+
+	public List<Tecnico> findAll() {		
+		return tecnicoRepository.findAll();
 	}
 }
